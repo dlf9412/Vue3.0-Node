9f11373605904f33208bfd0c355595db78372481
dlf <1055205416@qq.com>
2022-05-20 17:27:45 +0800

    feat(优化代码格式):

--- vue-ts/src/view/Home/index.vue
+++ vue-ts/src/view/Home/index.vue
@@ -7,8 +7,8 @@
           <template v-slot>
              <div class="main-header-left">
                 <div id="menu-logo"></div>
-                <div>{{saasConfig.title}}</div>
-                <div></div>
+                <div class="menu-saas">{{saasConfig.title}}</div>
+                <div>{{company.name}}</div>
              </div>
           </template>
         </Header>
@@ -18,22 +18,14 @@
     </Layout>
   </div>
 </template>
-<script setup='props,{emit}' lang='ts'>
+<script setup lang='ts'>
 import axios from 'axios'
 import {useRouter} from 'vue-router'
 import {Layout,LayoutHeader,LayoutContent} from 'ant-design-vue'
 import Header from './header.vue'
 import { onMounted, reactive } from '@vue/runtime-core'
 const router=useRouter()
-const routeClick=()=>{
-  router.push('/app')
-}
-axios.get('/api').then((res)=>{
-  console.log(res)
-})
-onMounted(()=>{
-  console.log(1111)
-})
+
 const saasConfig=reactive({
     title:'SaaS平台',
     url:''
@@ -42,29 +34,52 @@ const company=reactive({
     name:'tony'
   })
 
+const routeClick=()=>{
+  router.push('/app')
+}
+
+axios.get('/api').then((res)=>{
+  console.log(res)
+})
+onMounted(()=>{
+  // console.log(1111)
+})
+
+
 </script>
-<style lang="less">
+<style lang="less" scoped>
 @import '@/common/css/index.less';
 .ant-layout-header, .ant-layout-footer {
   color: black;
   background: #fff;
-  padding: 0 15px;
+}
+.ant-layout-header{
+  padding-left: 10px;
+  padding-right: 30px;
 }
 .home-index{
+  height: 100%;
+  /deep/ .ant-layout{
+    height: 100%;
+  }
   .main-header-left{
       .displayFlex(flex-start);
-      div{
-        margin-right: 10px;
-      }
       #menu-logo{
         background: url('@/assets/menu-logo.png') no-repeat 50%;
         background-size: 100% 100%;
         width: 44px;
         height: 40px;
-        margin: 0 auto;
         line-height: 40px;
         color: #fff;
         text-align: center;
+        cursor: auto;
+      }
+      .menu-saas:hover{
+        color: #00aeff;
+      }
+      div{
+        margin-right: 20px;
+        cursor: pointer;
       }
     }
 }
